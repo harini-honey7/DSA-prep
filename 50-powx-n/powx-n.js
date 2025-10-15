5414fef976c5f46954bd9a8aceb860d7cf549e57
Harini <saiharinikona@gmail.com>
2025-10-15 18:48:19 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (30.94%) - LeetSync

---
+++ 50-powx-n/powx-n.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(n===0){
+        return 1;
+    }
+    if(n<0){
+        x=1/x;
+        n=-n;
+    }
+    if(n%2===0){ 
+        let half=myPow(x,n/2);
+        return half*half;
+
+    }else{
+        return x*myPow(x,n-1);
+    }
+    
+};
