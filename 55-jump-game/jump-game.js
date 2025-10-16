db4e0ace67101b5c72a6f292e5c6a8e9ec95f571
Harini <saiharinikona@gmail.com>
2025-10-16 18:57:49 +0530

    Time: 1 ms (83.46%) | Memory: 60 MB (24.03%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let target = nums.length-1
+    for(let i=nums.length-1;i>=0;i--){
+        if(target<=i+nums[i]){
+            target=i;
+        }
+    }
+  return target===0?true:false;
+    
+};
